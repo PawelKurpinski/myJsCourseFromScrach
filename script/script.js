0d67a64e1d106b2ae644fbbab54eb028d0cd6991
PawelKurpinski <pkurpinski@wp.pl>
2017-11-29 22:19:45 +0100

    29.12

---
+++ script/script.js
@@ -0,0 +1,21 @@
+var x = 5;
+var y = 5;
+
+document.write(x % y);
+
+var z = 8;
+z = z + 4;
+
+document.write(z);
+
+
+function doSomething(x,y){
+    alert(x+y);
+}
+doSomething(1,3);
+
+
+a = 8;
+var czyParzysta (a % 2 === 0) ? true : false;
+
+alert
