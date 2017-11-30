448a05460f0d190489a89abdf884e98b5a08b5ff
PawelKurpinski <pkurpinski@wp.pl>
2017-11-30 23:47:14 +0100

    30.12

--- script/script.js
+++ script/script.js
@@ -15,7 +15,47 @@ function doSomething(x,y){
 doSomething(1,3);
 
 
-a = 8;
-var czyParzysta (a % 2 === 0) ? true : false;
+var a = 8;
+var czyParzysta = (a % 2 === 0) ? true : false;
+alert(czyParzysta);
 
-alert
+
+var b = 12;
+var czyWieksza = (a > b) ? "a większe od b" : "a mniejsze od b";
+alert(czyWieksza);
+
+alert((b % 2 === 0) ? "b jest parzyste" : "b jest nieparzyste");
+
+
+var dzienTygodnia = 12;
+
+switch (dzienTygodnia) {
+    case 1:
+        alert("dzisiaj jest poniedziałek");
+        break;
+    case 2:
+        alert("dzisiaj jest poniedziałek");
+        break;
+    case 3:
+        alert("dzisiaj jest poniedziałek");
+        break;
+    default:
+        alert("błędny numer! ogarnij się!!")
+}
+
+function sayIt(){
+    alert("do it");
+}
+sayIt();
+
+var x = function sayAgain(word){
+    alert(word)
+}
+x("test");
+
+var x = function sayAgain(word){
+    alert(word)
+}
+x(function(){
+    alert("something")
+});
