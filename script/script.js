a153572ae72d54f347d71a256e5d32568bc26af0
PawelKurpinski <pkurpinski@wp.pl>
2017-12-01 20:47:15 +0100

    01.12

--- script/script.js
+++ script/script.js
@@ -53,9 +53,52 @@ var x = function sayAgain(word){
 }
 x("test");
 
-var x = function sayAgain(word){
-    alert(word)
-}
-x(function(){
-    alert("something")
-});
+//
+//
+// var x = function sayAgain(word){
+//     alert(word)
+// }
+// x(function(){
+//     alert("something")
+// });
+//
+// function test(f){
+//     f();
+// }
+//
+// test(
+//     function(){
+//         alert("test");
+//     }
+// );
+//
+// /* jako argument przesyłam do funkcji drugą funkcję
+// Sam zaspis test( function...) oznacza niejako
+// var f = function(){ alert...}
+// Ale sam fakt przesłania wiele nie wnosi ponieważ potrzeba jeszcze wywołania teara z kolei funckji wcześniej przekazanej pod zmienną f a więc wywołania f();  */
+//
+// function test(f, y){
+//     f(10 + y);
+// }
+// test(
+//     function(x){
+//         alert("test"+ " "+ x);
+//     },
+//     20
+// );
+
+// tutaj poza powyżej widocznymi działaniami przekazuję jeszcze poza alertem wartość 20 jako drugi argument do funkcji test; co ważne jednak funkcja przekazywana do funkcji test ma argument x, który przy wywołaniu w linii 79 zostaję podstawiony lcizbą 10
+
+var h1 = function(type){
+    if (type === "szef")
+        return function(name){
+            alert("cześć szefie," + name + " !");
+        };
+    else return function(name){
+        alert("Cześć"+ name + " !");
+    };
+
+};
+var zwroconaFunkcja = h1("szef");
+
+zwroconaFunkcja("Paweł");
